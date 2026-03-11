ac84388d83d959bdfd26527ffed30d50be0a19ff
koduru-purnima <koduru.purnima@gmail.com>
2026-03-12 01:23:46 +0800

    4 patterns added

--- pattern1.py
+++ pattern1.py
@@ -1,11 +1,14 @@
 num  = int(input(" enter number of rows: "))
-print(' first Pattern :')
+print(' ---------first Pattern :--------')
 for i in range(1,num+1):
     print('* ' * i)
 print("----------Reverse Pattern ---------------")
 
-for i in range(num+1,1,-1):
+for i in range(num,0,-1):
     print('* ' * i)
 print("------------TRIANGLE-------------")
 for i in range(1,num+1):
     print(" "*(num-i)+'* '*i)
+print("------------Reverse TRIANGLE-------------")
+for i in range(num,0,-1):
+    print(' '*(num-i)+'* '*i)
