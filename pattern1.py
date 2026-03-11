12cfbd553c765396b0b341f9daf7d3dae3c34379
koduru-purnima <koduru.purnima@gmail.com>
2026-03-12 01:03:39 +0800

    first patterns

--- pattern1.py
+++ pattern1.py
@@ -2,6 +2,10 @@ num  = int(input(" enter number of rows: "))
 print(' first Pattern :')
 for i in range(1,num+1):
     print('* ' * i)
-print(' Reverse Pattern :')
+print("----------Reverse Pattern ---------------")
+
 for i in range(num+1,1,-1):
     print('* ' * i)
+print("------------TRIANGLE-------------")
+for i in range(1,num+1):
+    print(" "*(num-i)+'* '*i)
