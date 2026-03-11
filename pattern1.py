33eea8ac6ef6bf7c0f01832b159cccdfe5366c32
koduru-purnima <koduru.purnima@gmail.com>
2026-03-12 02:27:05 +0800

    added diff number triangle pattern to pattern1.py

--- pattern1.py
+++ pattern1.py
@@ -27,3 +27,10 @@ for i in range(1,num+1):
 print(' ---------Reverse number triangle Pattern :--------')
 for i in range(num,0,-1):
     print((str(i)+' ')* i)
+
+print(' --------- diff num triangle Pattern :--------')
+for i in range(num+1):
+    for j in range(1,i+1):
+        print(j,end=' ')
+
+    print(' \n')
