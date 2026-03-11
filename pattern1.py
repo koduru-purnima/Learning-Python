f76c7ae71fd032e32f02140258f61e404d3fb5a8
koduru-purnima <koduru.purnima@gmail.com>
2026-03-12 00:21:23 +0800

    first commit

---
+++ pattern1.py
@@ -0,0 +1,7 @@
+num  = int(input(" enter number of rows: "))
+print(' first Pattern :')
+for i in range(1,num+1):
+    print('* ' * i)
+print(' Reverse Pattern :')
+for i in range(num+1,1,-1):
+    print('* ' * i)
